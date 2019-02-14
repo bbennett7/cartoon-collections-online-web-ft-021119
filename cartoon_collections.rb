95081793c9818435b8ad72f988845399fee87bd3
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-14 03:51:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,8 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  
   cheese_types = ["cheddar", "gouda", "camembert"]
+    array.find do |ingredient|
+      cheese_types.
+    end
 end
