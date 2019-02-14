3cdb5ad0459d13293fce70d0e3b8b67aa1a2ffdb
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-14 03:48:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,13 +5,15 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.collect do |calls|
-    "#{calls.capitalize}!"
+  array.collect do |call|
+    "#{call.capitalize}!"
   end
 end
 
 def long_planeteer_calls(array)
-  array.any?
+  array.any? do |call|
+    call.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
